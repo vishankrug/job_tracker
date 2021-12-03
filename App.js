336caca5c3bbaf0fe4553c9d74d8eb0bff82f06b
vishankrug <vishankr@uw.edu>
2021-12-03 15:06:24 -0800

    startert

---
+++ App.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sessions from 'express-session';
+import MsIdExpress from 'microsoft-identity-express'
+
+
+
+export default app;
